bda217dd7150584ea42e116317ebecb415e29dc2
Carl <carllong223@gmail.com>
2022-11-14 11:46:46 +0000

    Updates

---
+++ precache-manifest.5435cf48e41598a835780120e84fd92b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b56d68b66e302100776a",
+    "url": "/movie-night/static/css/main.402ed8e7.chunk.css"
+  },
+  {
+    "revision": "b56d68b66e302100776a",
+    "url": "/movie-night/static/js/main.32cc145d.chunk.js"
+  },
+  {
+    "revision": "29d6abc94b60f4b3616a",
+    "url": "/movie-night/static/js/runtime~main.8d257a65.js"
+  },
+  {
+    "revision": "2f4517b6b2230aa2bbcc",
+    "url": "/movie-night/static/js/2.b66ba411.chunk.js"
+  },
+  {
+    "revision": "eedfb1b54cc50ef37ed5",
+    "url": "/movie-night/static/js/3.e8d7c53c.chunk.js"
+  },
+  {
+    "revision": "23a3e4237f142d8ea8f36bddabb1b7a4",
+    "url": "/movie-night/index.html"
+  }
+];
